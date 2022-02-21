2c584ba442a9b23db7c9111a30e7a1699429ad06
Jamaal Henry <jamaalhenry30@gmail.com>
2022-02-20 19:18:41 -0500

    made corrections to errors with naming conventions

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
-import os
-from flask_wtf import Flaskform
-from wtforms import StringField, TextField
+
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField
 from wtforms.validators import DataRequired,Email
 
 
-class ContactForm(Flaskform):
+class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email= StringField('E-mail', validators=[DataRequired(),Email()])
     subject = StringField('Subject', validators=[DataRequired()])
-    message = TextField('Message', validators=[DataRequired()])
+    message = TextAreaField('Message', validators=[DataRequired()])
